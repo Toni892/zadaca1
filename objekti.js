5df92d591b79052d0e3b75b0cd103b23d363e910
Toni892 <54323949+Toni892@users.noreply.github.com>
2024-02-11 15:49:10 +0100

    Add files via upload

---
+++ objekti.js
@@ -0,0 +1,13 @@
+var korpa = {
+    SSD: 500,
+    "Maticna ploca": 450,
+};
+
+console.log(korpa["SSD"]);
+console.log(korpa["Maticna ploca"]);
+
+korpa["Graficka"] = 520;
+korpa["Napajanje"] = 150;
+korpa["RAM Memorija"] = 200;
+
+console.log(korpa);
---
+++ script.js
@@ -0,0 +1,14 @@
+var proizvodi = ["Maticna ploca", "Ram memorija", "SSD"]
+console.log(proizvodi[0]);
+
+proizvodi[3] = "HDD";
+console.log(proizvodi);
+
+proizvodi.push("GPU", "Floppy disk", "CD Rom");
+console.log(proizvodi);
+
+proizvodi[2] = "kuciste";
+console.log(proizvodi);
+
+proizvodi.push("Napajanje");
+console.log(proizvodi);
